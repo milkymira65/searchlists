fed7f9d0e28f9ab4e53bf8a16117967be49f42ba
milky <milkymira@gmail.com>
2023-01-11 12:43:41 +0700

    updaate2

--- src/component/ToDo.js
+++ src/component/ToDo.js
@@ -14,79 +14,189 @@ function ToDo() {
   return (
     <Box w="80%" mt="3%" ml="8%">
       <Center>
-        <Heading
-          m="5%"
-          // onClick={() => {
-          //   handleAddCounter(counter);
-          // }}
-        >
-          Test
-        </Heading>
+        <Heading m="5%">Test</Heading>
       </Center>
       <Wrap spacing="30px" justify="center">
         <WrapItem>
-          <Center w="180px" h="80px" bg="blackAlpha.100">
+          <Center
+            w="180px"
+            h="80px"
+            bg="blackAlpha.100"
+            rounded={"full"}
+            _hover={{
+              textDecoration: "none",
+              bg: "#3D6356",
+            }}
+          >
             Male
           </Center>
         </WrapItem>
         <WrapItem>
-          <Center w="180px" h="80px" bg="blackAlpha.100">
+          <Center
+            w="180px"
+            h="80px"
+            bg="blackAlpha.100"
+            rounded={"full"}
+            _hover={{
+              textDecoration: "none",
+              bg: "#3D6356",
+            }}
+          >
             Female
           </Center>
         </WrapItem>
         <WrapItem>
-          <Center w="180px" h="80px" bg="blackAlpha.100">
+          <Center
+            w="180px"
+            h="80px"
+            bg="blackAlpha.100"
+            rounded={"full"}
+            _hover={{
+              textDecoration: "none",
+              bg: "#3D6356",
+            }}
+          >
             Bigender
           </Center>
         </WrapItem>
         <WrapItem>
-          <Center w="180px" h="80px" bg="blackAlpha.100">
+          <Center
+            w="180px"
+            h="80px"
+            bg="blackAlpha.100"
+            rounded={"full"}
+            _hover={{
+              textDecoration: "none",
+              bg: "#3D6356",
+            }}
+          >
             Non-binary
           </Center>
         </WrapItem>
         <WrapItem>
-          <Center w="180px" h="80px" bg="blackAlpha.100">
+          <Center
+            w="180px"
+            h="80px"
+            bg="blackAlpha.100"
+            rounded={"full"}
+            _hover={{
+              textDecoration: "none",
+              bg: "#3D6356",
+            }}
+          >
             Agender
           </Center>
         </WrapItem>
 
         <WrapItem>
-          <Center w="180px" h="80px" bg="blackAlpha.100">
+          <Center
+            w="180px"
+            h="80px"
+            bg="blackAlpha.100"
+            rounded={"full"}
+            _hover={{
+              textDecoration: "none",
+              bg: "#3D6356",
+            }}
+          >
             Polygender
           </Center>
         </WrapItem>
         <WrapItem>
-          <Center w="180px" h="80px" bg="blackAlpha.100">
+          <Center
+            w="180px"
+            h="80px"
+            bg="blackAlpha.100"
+            rounded={"full"}
+            _hover={{
+              textDecoration: "none",
+              bg: "#3D6356",
+            }}
+          >
             Genderfluid
           </Center>
         </WrapItem>
         <WrapItem>
-          <Center w="180px" h="80px" bg="blackAlpha.100">
+          <Center
+            w="180px"
+            h="80px"
+            bg="blackAlpha.100"
+            rounded={"full"}
+            _hover={{
+              textDecoration: "none",
+              bg: "#3D6356",
+            }}
+          >
             Genderqueer
           </Center>
         </WrapItem>
         <WrapItem>
-          <Center w="180px" h="80px" bg="blackAlpha.100">
+          <Center
+            w="180px"
+            h="80px"
+            bg="blackAlpha.100"
+            rounded={"full"}
+            _hover={{
+              textDecoration: "none",
+              bg: "#3D6356",
+            }}
+          >
             BraZil
           </Center>
         </WrapItem>
         <WrapItem>
-          <Center w="180px" h="80px" bg="blackAlpha.100">
+          <Center
+            w="180px"
+            h="80px"
+            bg="blackAlpha.100"
+            rounded={"full"}
+            _hover={{
+              textDecoration: "none",
+              bg: "#3D6356",
+            }}
+          >
             Thailand
           </Center>
         </WrapItem>
         <WrapItem>
-          <Center w="180px" h="80px" bg="blackAlpha.100">
+          <Center
+            w="180px"
+            h="80px"
+            bg="blackAlpha.100"
+            rounded={"full"}
+            _hover={{
+              textDecoration: "none",
+              bg: "#3D6356",
+            }}
+          >
             Sweden
           </Center>
         </WrapItem>
         <WrapItem>
-          <Center w="180px" h="80px" bg="blackAlpha.100">
+          <Center
+            w="180px"
+            h="80px"
+            bg="blackAlpha.100"
+            rounded={"full"}
+            _hover={{
+              textDecoration: "none",
+              bg: "#3D6356",
+            }}
+          >
             Japan
           </Center>
         </WrapItem>
         <WrapItem>
-          <Center w="180px" h="80px" bg="blackAlpha.100">
+          <Center
+            w="180px"
+            h="80px"
+            bg="blackAlpha.100"
+            rounded={"full"}
+            _hover={{
+              textDecoration: "none",
+              bg: "#3D6356",
+            }}
+          >
             Netherlands
           </Center>
         </WrapItem>
